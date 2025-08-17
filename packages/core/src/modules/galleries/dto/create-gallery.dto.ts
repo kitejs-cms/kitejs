10b900e2dc41aa9d2b01db57d1c77a86f3cc86e7
tascaenzo <enzotasca.dev@gmail.com>
2025-08-17 19:59:27 +0200

    Revert "feat(gallery): add dashboard and api for galleries (#9)"
    This reverts commit 46b7e0cbafa97dffe34c30331dab71d869672cb8.

--- packages/core/src/modules/galleries/dto/create-gallery.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsArray, IsString } from 'class-validator';
-
-export class CreateGalleryDto {
-  @IsString()
-  title: string;
-
-  @IsArray()
-  images: string[];
-}
--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -1,21 +1,14 @@
-import globals from "globals";
-import { config as baseConfig } from "./base.js";
-
 /** @type {import("eslint").Linter.Config} */
-export const config = [
-  ...baseConfig,
-  {
-    rules: {
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
+module.exports = {
+  extends: ['./base.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-];
+  env: {
+    node: true,
+    jest: true,
+  },
+};
