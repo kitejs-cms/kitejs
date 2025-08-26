2b81366cfd9aa53ec2fe1b37f257ad20def9b99c
tascaenzo <enzo@Mac-mini-di-enzo.local>
2025-08-26 22:07:36 +0200

    fix publish script

--- apps/blog/src/main.ts
+++ apps/blog/src/main.ts
@@ -1,5 +1,5 @@
 import { AppModule } from "./app.module";
 import { bootstrap } from "@kitejs-cms/core/index";
-import { GalleryPlugin } from "@kitejs-cms/plugin-gallery-api/plugin.config";
+import { GalleryPlugin } from "@kitejs-cms/plugin-gallery-api";
 
 bootstrap({ modules: [AppModule], plugins: [GalleryPlugin] });
