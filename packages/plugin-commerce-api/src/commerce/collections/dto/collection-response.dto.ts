28a6d78551a3662da974fdf320f97dc675717783
tascaenzo <enzotasca.dev@gmail.com>
2025-09-18 10:04:51 +0200

    Add validation decorators to response DTOs

--- packages/plugin-commerce-api/src/commerce/collections/dto/collection-response.dto.ts
+++ packages/plugin-commerce-api/src/commerce/collections/dto/collection-response.dto.ts
@@ -1,21 +1,30 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsDate, IsObject, IsString } from "class-validator";
 
 import type { CollectionResponseModel } from "../models/collection-response.model";
 
 export class CollectionResponseDto implements CollectionResponseModel {
   @ApiProperty()
+  @IsString()
   declare id: string;
 
   @ApiProperty({ type: Object })
+  @IsObject()
   declare slugs: Record<string, string>;
 
   @ApiProperty({ type: Object })
+  @IsObject()
   declare translations: Record<string, Record<string, unknown>>;
 
   @ApiProperty()
+  @Type(() => Date)
+  @IsDate()
   declare createdAt: Date;
 
   @ApiProperty()
+  @Type(() => Date)
+  @IsDate()
   declare updatedAt: Date;
 
   constructor(partial: CollectionResponseModel) {
--- packages/plugin-commerce-api/src/commerce/customer-addresses/dto/customer-address-response.dto.ts
+++ packages/plugin-commerce-api/src/commerce/customer-addresses/dto/customer-address-response.dto.ts
@@ -1,4 +1,13 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import {
+  IsBoolean,
+  IsDate,
+  IsMongoId,
+  IsOptional,
+  IsString,
+} from "class-validator";
+
 import { CustomerAddress } from "../schemas/customer-address.schema";
 import type { CustomerAddressResponseModel } from "../models/customer-address-response.model";
 
@@ -6,54 +15,81 @@ export class CustomerAddressResponseDto
   implements CustomerAddressResponseModel
 {
   @ApiProperty()
+  @IsMongoId()
   id: string;
 
   @ApiProperty({ description: "Identifier of the customer (core user)" })
+  @IsMongoId()
   userId: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   label?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   firstName?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   lastName?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   company?: string;
 
   @ApiProperty()
+  @IsString()
   address1: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   address2?: string;
 
   @ApiProperty()
+  @IsString()
   city: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   postalCode?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   province?: string;
 
   @ApiProperty()
+  @IsString()
   countryCode: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   phone?: string;
 
   @ApiProperty()
+  @IsBoolean()
   isDefaultShipping: boolean;
 
   @ApiProperty()
+  @IsBoolean()
   isDefaultBilling: boolean;
 
   @ApiProperty()
+  @Type(() => Date)
+  @IsDate()
   createdAt: Date;
 
   @ApiProperty()
+  @Type(() => Date)
+  @IsDate()
   updatedAt: Date;
 
   constructor(partial: CustomerAddress) {
