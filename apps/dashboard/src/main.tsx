0429fb0f23f824cb6ec70072500e1908135b624a
tascaenzo <enzo@Mac-mini-di-enzo.local>
2025-09-19 22:11:22 +0200

    fix build dashboard

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 import { DashboardProvider } from "@kitejs-cms/dashboard-core/dashboard-provider";
 import { GalleryModule } from "@kitejs-cms/plugin-gallery-dashboard";
 import { AnalyticsModule } from "@kitejs-cms/plugin-analytics-dashboard";
+import { CommerceModule } from "@kitejs-cms/plugin-commerce-dashboard";
 
 /* CSS */
 import "./index.css";
 import "@kitejs-cms/dashboard-core/styles.css";
 
 createRoot(document.getElementById("root")!).render(
-  <DashboardProvider modules={[GalleryModule, AnalyticsModule]} />
+  <DashboardProvider
+    modules={[GalleryModule, AnalyticsModule, CommerceModule]}
+  />
 );
--- packages/plugin-commerce-dashboard/src/index.ts
+++ packages/plugin-commerce-dashboard/src/index.ts
@@ -1,7 +1,3 @@
-export * from "./module";
-export * from "./constants";
-export * from "./components/commerce-settings";
-export * from "./pages/commerce-products";
-export * from "./pages/commerce-product-details";
-export * from "./pages/commerce-orders";
-export * from "./pages/commerce-order-details";
+import "./styles.css";
+
+export { CommerceModule } from "./module";
