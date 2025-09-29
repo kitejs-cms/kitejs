c69a9e3642256f2c631d2e6d93c4fc669da15199
tascaenzo <enzotasca.dev@gmail.com>
2025-09-29 22:49:56 +0200

    refactor: modularize products translations

--- packages/plugin-commerce-dashboard/src/locales/en/index.ts
+++ packages/plugin-commerce-dashboard/src/locales/en/index.ts
@@ -1,6 +1,6 @@
 import shared from "./shared.json";
 import settings from "./settings.json";
-import products from "./products.json";
+import products from "./products";
 import orders from "./orders.json";
 import collections from "./collections";
 
